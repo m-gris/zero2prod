b8dd3d7801b7164b7170bb6b293dcc345131da18
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-11-05 07:23:09 +0800

    chore: some comments on rust syntax etc...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // NOTE: let is a STATEMENT that binds the expression "Hello" (&str) to msg
+    // requires ; at the end
+    let msg = "Hello, you... Wonderful world!";
+    // NOTE:
+    // the ! in println! indicates that is is a Macro, not a Function !!!
+    // the ; at the end indicates a STATEMENT.
+    //  println! treats its first argument as a format string that may contain placeholders.
+    println!("{}", msg);
 }
