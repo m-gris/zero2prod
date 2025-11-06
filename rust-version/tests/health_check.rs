99fa40f779445d811a84d4386b8d26cdca86c5b4
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-11-07 06:42:21 +0800

    test: add integration test for health check endpoint
    - Implement basic health_check_works test
    - Configure Cargo to discover health_check test file in custom rust-version/tests/ directory. (we might revert back to the conventional layout though...)
    - Add `reqwest` dependency for HTTP client testing.

--- rust-version/tests/health_check.rs
+++ rust-version/tests/health_check.rs
@@ -1,7 +1,28 @@
 //! tests/health_check.rs
 
-use zero2prod::main;
-#[test]
-fn dummy_test() {
-    main()
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute. //
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    // ARRANGE
+    spawn_app().await.expect("Failed to spawn our App.");
+    // use REQWEST to perform HTTP requests against our app
+    let client = reqwest::Client::new();
+
+    // ACT
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // ASSERT
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> Result<(), std::io::Error> {
+    zero2prod::run().await
 }
