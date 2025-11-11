fc59eafa9a11807b357da691f595fa57e3b83bf2
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-11-11 08:02:50 +0800

    feat: add app user creation to init_db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -17,3 +17,20 @@ docker run \
     --detach \
     --name "${CONTAINER_NAME}" \
     postgres -N 1000 # max number of connections (for testing purposes)
+
+
+# WARNING: By default, Postgres launches with a superuser named postgres, owner of a default database named postgres.
+# It is a good practice to avoid using the superuser for our applications, as it has too many privileges.
+# NOTE: create a dedicated application user, with enough privileges to create its own database
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=newsletter}"
+
+# SQL
+# CREATE THE APPLICATION USER
+CREATE_USER_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_USER_QUERY}"
+
+# GRANT DB PRIVILEGES TO THE APP USER
+GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
